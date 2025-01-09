5ab8e397a967f92ec888f3b72727bf3b043be0db
rupin_desai <rupin360@gmail.com>
2025-01-09 21:25:46 +0530

    `Added hero background image and updated Hero component styles`

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,39 +1,45 @@
 import React from "react";
-import { FaArrowRight } from "react-icons/fa"; // Importing the arrow icon
 import hero from "../assets/hero.png";
 import HeroStats from "./HeroStats";
 import HeroTitle from "./HeroTitle";
-
+import HeroBg from "../assets/hero-bg.png"
 
 const Hero = () => {
   return (
-    <section className="bg-white h-screen flex items-center justify-between relative flex-row ">
-    {/* Left Section */}
-    <div className="w-full md:w-7/12 text-center md:text-left pl-6 md:pl-32 flex flex-col justify-between h-full">
-      <HeroTitle
-        title="Architectural Solutions for Every Vision"
-        subtitle="Explore the symbiotic relationship between architecture and the environment"
-      />
+    <section
+      className="bg-white h-screen flex items-center justify-between relative flex-row"
+      style={{
+        backgroundImage: `url(${HeroBg})`, // Add your vector image path here
+        backgroundRepeat: 'no-repeat',
+        backgroundSize: 'cover',
+        backgroundPosition: 'center',
+      }}
+    >
+      {/* Left Section */}
+      <div className="w-full md:w-7/12 text-center md:text-left pl-6 md:pl-32 flex flex-col justify-between h-full">
+        <HeroTitle
+          title="Architectural Solutions for Every Vision"
+          subtitle="Explore the symbiotic relationship between architecture and the environment"
+        />
   
-      {/* Bottom Text with Overlap */}
-      <HeroStats
-        clients={14}
-        projectsCompleted={21}
-        awardsWon={6}
-        ideaText="Make it!"
-      />
-    </div>
+        {/* Bottom Text with Overlap */}
+        <HeroStats
+          clients={14}
+          projectsCompleted={21}
+          awardsWon={6}
+          ideaText="Make it!"
+        />
+      </div>
   
-    {/* Right Section - Image */}
-    <div className="w-full md:w-5/12 h-full bg-cover bg-center flex justify-center md:justify-start">
-      <img
-        src={hero}
-        alt="ArchDesign Logo"
-        className="h-full w-full object-cover md:w-auto"
-      />
-    </div>
-  </section>
-  
+      {/* Right Section - Image */}
+      <div className="w-full md:w-5/12 h-full bg-cover bg-center flex justify-center md:justify-start">
+        <img
+          src={hero}
+          alt="ArchDesign Logo"
+          className="h-full w-full object-cover md:w-auto"
+        />
+      </div>
+    </section>
   );
 };
 
