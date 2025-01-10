a9e07257126b17315c89de2869c1e95ca7642a66
rupin_desai <rupin360@gmail.com>
2025-01-10 19:25:23 +0530

    `Made About Header Responsive`

--- src/components/AboutHeader.jsx
+++ src/components/AboutHeader.jsx
@@ -3,10 +3,10 @@ import { motion } from "framer-motion";
 
 const AboutHeader = ({ headerText }) => {
   return (
-    <section className="flex justify-end bg-white py-16 pl-48 pr-20">
-      <div className="flex justify-center text-center align-middle px-10 my-auto">
+    <section className="flex flex-col lg:flex-row justify-center bg-white py-8 px-4 sm:px-6 md:px-12 lg:px-20">
+      <div className="flex justify-center text-center my-auto w-full">
         <motion.hr
-          className="flex border-t-4 border-green-900 rounded-l-full rounded-r-full"
+          className="flex border-t-4 mt-5 border-green-900 rounded-l-full sm:mt-5 rounded-r-full mx-auto"
           initial={{ width: 0 }}
           whileInView={{ width: "20rem" }}
           transition={{ duration: 0.5 }}
@@ -14,13 +14,15 @@ const AboutHeader = ({ headerText }) => {
         />
       </div>
       <motion.div
-        className="flex p-0 text-lg text-gray-custom"
+        className="flex p-0 text-lg text-gray-custom mt-4 lg:mt-0 w-full"
         initial={{ x: "100%", opacity: 0 }} // Start from the right with 0 opacity
         whileInView={{ x: 0, opacity: 1 }} // Slide to the center and fade in
         transition={{ duration: 1 }}
         viewport={{ once: true }} // Ensures the animation happens only once when the element comes into view
       >
-        {headerText}
+        <div className="text-center sm:text-left text-sm sm:text-2xl md:text-lg lg:text-xl  text-black">
+          {headerText}
+        </div>
       </motion.div>
     </section>
   );
